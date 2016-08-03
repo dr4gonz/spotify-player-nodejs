487721ef6f06b021def232e63f42bb07a12270cf
Carl Egbert and Matthew Reyes <carl+matthew@>
2016-08-03 15:51:12 -0700

    add artist search and methods

--- js/spotify-search-interface.js
+++ js/spotify-search-interface.js
@@ -17,6 +17,25 @@ var artistDisplay = function(albumId, artistName) {
   $("#artist-field"+albumId).text(artistName);
 };
 
+var showArtist = function(artist) {
+  console.log(artist);
+  var artistId = artist.id;
+  $("#artist-image").empty();
+  $("#spotify-results").empty();
+  $("#artist-image").append("<h3>"+artist.name+"</h3><img src='"+artist.images[2].url+"'>");
+};
+
+var albumDisplay = function(album) {
+  console.log(album);
+  $("#spotify-results").append("<img src='"+album.images[2].url+"'><li id='album"+album.id+"'><a  class='widget-link'>"+album.name+"</a></li>");
+
+  $('#album'+album.id).click(function(){
+    console.log(album.id);
+    $("#player-widget").empty();
+    $("#player-widget").append('<iframe src="https://embed.spotify.com/?uri=spotify:album:' + album.id + '" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>');
+  });
+};
+
 $(document).ready(function(){
 
   var currentSpotifySearch = new SpotifySearch();
@@ -25,6 +44,11 @@ $(document).ready(function(){
     event.preventDefault();
     var albumSearchTerm = $("#album").val();
     currentSpotifySearch.searchAlbum(albumSearchTerm, showAlbum, artistDisplay);
+  });
+  $("#artist-search").submit(function(event){
+    event.preventDefault();
+    var artistSearchTerm = $("#artist").val();
+    currentSpotifySearch.searchArtist(artistSearchTerm, showArtist, albumDisplay);
   });
 
 
--- js/spotify-search.js
+++ js/spotify-search.js
@@ -2,15 +2,23 @@ SpotifySearch = function() {};
 
 SpotifySearch.prototype.searchAlbum = function(searchParameter, showAlbum, artistDisplay) {
   var that = this;
-  $.get("https://api.spotify.com/v1/search?q="+searchParameter+"&type=album").then(function(response){
+  $.get("https://api.spotify.com/v1/search?q="+searchParameter+"&limit=10&type=album").then(function(response){
     console.log(response);
-    var upperLimit;
-    if (response.albums.items.length > 10) upperLimit = 10;
-    else upperLimit = response.albums.items.length;
-    for (i = 0; i< upperLimit; i++) {
+    for (i = 0; i< response.albums.items.length; i++) {
       showAlbum(response.albums.items[i]);
       that.getArtist(response.albums.items[i].id, artistDisplay);
     }
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
+SpotifySearch.prototype.searchArtist = function(searchParameter, showArtist, albumDisplay) {
+  var that=this;
+  $.get("https://api.spotify.com/v1/search?q="+searchParameter+"&limit=1&type=artist").then(function(response){
+    console.log(response);
+    showArtist(response.artists.items[0]);
+    that.getAlbums(response.artists.items[0].id, albumDisplay);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
@@ -25,4 +33,17 @@ SpotifySearch.prototype.getArtist = function(albumId, artistDisplay) {
   });
 };
 
+SpotifySearch.prototype.getAlbums = function(artistId, albumDisplay) {
+  $.get("https://api.spotify.com/v1/artists/"+artistId+"/albums?limit=10&album_type=album").then(function(response){
+    console.log(response);
+    for(i = 0; i < response.items.length; i++) {
+      albumDisplay(response.items[i]);
+    }
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
 exports.searchModule = SpotifySearch;
+//search for artist
+//find artists albums by artist id
